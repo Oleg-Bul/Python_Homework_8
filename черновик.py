b56747827790e085b5167c8a8b491da5f3872243
Oleg_bul <kokseg63@gmail.com>
2022-12-20 00:42:27 +0400

    finale

--- черновик.py
+++
@@ -1,8 +0,0 @@
-# with open('history.txt') as file:
-#     lines = [line.rstrip() for line in file]
-# print(lines)
-
-
-with open('history.txt') as f:
-    lines = [line.rstrip('\n') for line in f]
-print(lines)
